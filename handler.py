458b89aaa46a0c36ef974d1b07e4764a937c8497
andreas-fd <andreas.iatropoulos@fanduel.com>
2017-12-12 20:48:19 +0000

    use SlackClient

--- handler.py
+++ handler.py
@@ -1,9 +1,10 @@
 import json
 import os
 import logging
-import urllib
 
 # Grab the Bot OAuth token from the environment.
+from slackclient import SlackClient
+
 BOT_TOKEN = "xoxb-286531350151-MFLesbsehzDsxmnybsYqJQCo"
 
 SLACK_URL = "https://slack.com/api/chat.postMessage"
@@ -15,33 +16,14 @@ def handle_miss_ping(event, context):
     text = slack_event['text']
     channel_id = slack_event["channel"]
 
-    data = urllib.parse.urlencode(
-        (
-            ("token", BOT_TOKEN),
-            ("channel", channel_id),
-            ("text", text)
-        )
+    if "bot_id" in slack_event:
+        return
+
+    sc = SlackClient(BOT_TOKEN)
+
+    sc.api_call(
+        "chat.postMessage",
+        channel=channel_id,
+        text=text,
     )
-    data = data.encode("ascii")
-    request = urllib.request.Request(
-        SLACK_URL,
-        data=data,
-        method="POST"
-    )
-
-    urllib.request.urlopen(request).read()
-
-    return "200 OK"
-
-    # body = {
-    #     "message": "Hello slack! We are ready for pingo pong!",
-    #     "input": event
-    # }
-    #
-    # response = {
-    #     "statusCode": 200,
-    #     "body": json.dumps(body)
-    # }
-    #
-    # return response
 
