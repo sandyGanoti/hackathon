e59b01a1fa04c3cdbccc02358a961ea2f68ad90d
andreas-fd <andreas.iatropoulos@fanduel.com>
2017-12-13 18:16:50 +0000

    move to correct lambda

--- current_game.py
+++ current_game.py
@@ -1,32 +1,22 @@
 from __future__ import print_function
 import json
 import os
-from slackclient import SlackClient
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+from game_repo import GameRepository
+
 BOT_NAME = "Miss Ping"
 PING_PONG_CHANNEL = "C8E4EUGGM"
 
+NEO4J_EC2_IP = os.environ["NEO4J_EC2_IP"]
+NEO4J_USER = os.environ["NEO4J_USER"]
+NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
+
 
 def request_current_game(event, context):
-    print("event: {}".format(event))
-    sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
-
-    if "challenge" in sns_message:
-        return sns_message["challenge"]
-
-    slack_event = sns_message['event']
-    if "bot_id" in slack_event:
-        return
-
-    text = slack_event['text']
-    channel_id = slack_event['channel']
-    user = slack_event['user']
-
-    sc = SlackClient(BOT_TOKEN)
-    response = sc.api_call(
-        "chat.postMessage",
-        channel=PING_PONG_CHANNEL,
-        text=text,
+    print(event)
+    repository = GameRepository(
+        db_instance_ip=NEO4J_EC2_IP,
+        db_user=NEO4J_USER,
+        db_password=NEO4J_PASSWORD
     )
-    return response
+    return repository.get_current_game()
--- res_current_game.py
+++ res_current_game.py
@@ -1,12 +1,6 @@
 from __future__ import print_function
-import json
+
 import os
-
-from game_repo import GameRepository
-
-NEO4J_EC2_IP = os.environ["NEO4J_EC2_IP"]
-NEO4J_USER = os.environ["NEO4J_USER"]
-NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
 
 # topic arn
 ARN_WANT_TO_PLAY = "arn:aws:sns:us-east-1:580803390928:request_want_to_play"
@@ -16,9 +10,4 @@ ARN_CURRENT_GAME = "arn:aws:sns:us-east-1:580803390928:request_current_game"
 
 def response_current_game(event, context):
     print(event)
-    repository = GameRepository(
-        db_instance_ip=NEO4J_EC2_IP,
-        db_user=NEO4J_USER,
-        db_password=NEO4J_PASSWORD
-    )
-    return repository.get_current_game()
+    return event
