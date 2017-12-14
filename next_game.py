dce12f583299075f522ac88b8fd29c3de88d10be
sandyGanoti <sandy.ganoti@gmail.com>
2017-12-14 14:17:37 +0000

    finish the game

--- next_game.py
+++ next_game.py
@@ -6,9 +6,6 @@ import boto3
 
 from game_repo import GameRepository
 
-BOT_NAME = "Miss Ping"
-PING_PONG_CHANNEL = "C8E4EUGGM"
-
 NEO4J_EC2_IP = os.environ["NEO4J_EC2_IP"]
 NEO4J_USER = os.environ["NEO4J_USER"]
 NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
@@ -17,18 +14,21 @@ ARN_RES_NEXT_GAME = "arn:aws:sns:us-east-1:580803390928:response_next_game"
 
 
 def request_next_game(event, context):
-    print(event)
+    print("event: {}".format(event))
+    user_name = event["Records"][0]["Sns"]["Message"]
+    print(user_name)
     repository = GameRepository(
         db_instance_ip=NEO4J_EC2_IP,
         db_user=NEO4J_USER,
         db_password=NEO4J_PASSWORD
     )
-    players = repository.get_players_from_current_game() #TODO: set the right db method
-    print(players)
+    success = repository.finish_current_game(user_name)
+    print("success: {}".format(success))
     sns_client = boto3.client('sns')
+    sns_message = {"success": success}
     sns_response = sns_client.publish(
             TopicArn=ARN_RES_NEXT_GAME,
-            Message=json.dumps({"players": players})
+            Message=json.dumps(sns_message)
         )
 
     return {
--- res_next_game.py
+++ res_next_game.py
@@ -6,18 +6,22 @@ import os
 from slackclient import SlackClient
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_NAME = "Miss Ping"
 PING_PONG_CHANNEL = "C8E4EUGGM"
 
 
 def response_next_game(event, context):
     print(event)
-
     sns_msg = json.loads(event["Records"][0]["Sns"]["Message"])
     sc = SlackClient(BOT_TOKEN)
+
+    success = sns_msg["success"]
+
+    slack_msg = "The game finished!" if success else "You can't finish a game that is not yours :wave: "
     response = sc.api_call(
         "chat.postMessage",
         channel=PING_PONG_CHANNEL,
-        text=", ".join(sns_msg["players"]),
+        text=slack_msg,
     )
     print(response)
     return response
