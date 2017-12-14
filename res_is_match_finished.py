ca1c91e0230d3497a128b1f5a939e09533ffc179
sandyGanoti <sandy.ganoti@gmail.com>
2017-12-14 12:35:04 +0000

    response is match finished

--- res_is_match_finished.py
+++ res_is_match_finished.py
@@ -13,10 +13,13 @@ def response_is_match_finished(event, context):
     print(event)
     sns_msg = json.loads(event["Records"][0]["Sns"]["Message"])
     sc = SlackClient(BOT_TOKEN)
+
+    finished = sns_msg["finished"]
+    slack_msg = "Current game finished" if finished else "Current game has not finished yet"
     response = sc.api_call(
             "chat.postMessage",
             channel=PING_PONG_CHANNEL,
-            text=", ".join(sns_msg["players"]),
+            text=slack_msg,
         )
     print(response)
     return response
